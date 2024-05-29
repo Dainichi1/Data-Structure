e4a268ec2d092deff4c8e76f523f85c239c41013
dainichi79 <96421043+dainichi79@users.noreply.github.com>
2024-05-29 17:41:25 +0200

    Aggiunto "Is String Palindrome?"

--- src/Capitolo_009_Is_String_Palindrome_Timestamp_03_34/Palindrome.java
+++ src/Capitolo_009_Is_String_Palindrome_Timestamp_03_34/Palindrome.java
@@ -2,20 +2,19 @@ package Capitolo_009_Is_String_Palindrome_Timestamp_03_34;
 
 public class Palindrome {
 
-    public boolean isPalindrome (String word ) {
+    public void isPalindrome (String word ) {
         char[] charArray = word.toCharArray();
         int start = 0;
         int end = word.length() - 1;
         while (start < end) {
             if (charArray[start] != charArray[end]) {
                 System.out.println(word.toUpperCase() + " is not a palindrome word");
-                return false;
+                return;
             }
             start++;
             end --;
         }
         System.out.println(word.toUpperCase() + " is a palindrome word");
-        return true;
     }
     public static void main(String[] args) {
         Palindrome example = new Palindrome();
