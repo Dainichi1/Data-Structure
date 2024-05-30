0a53d60c0b048b471c494168c30b87ad3daa1639
dainichi79 <96421043+dainichi79@users.noreply.github.com>
2024-05-30 12:19:13 +0200

    Aggiunto "How to print element of a Singly Linked List"

---
+++ src/Singly_Linked_List/SinglyLinkedList.java
@@ -0,0 +1,30 @@
+package Singly_Linked_List;
+public class SinglyLinkedList {
+
+    private ListNode head;
+
+    private static class ListNode {
+        private int data; // generic type
+        private ListNode next;
+
+        public ListNode(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static void main(String[] args) {
+        SinglyLinkedList sll = new SinglyLinkedList();
+        sll.head = new ListNode(10);
+        ListNode second = new ListNode(1);
+        ListNode third = new ListNode(8);
+        ListNode fourth = new ListNode(11);
+
+        // Le collego insieme per formare una catena
+        sll.head.next = second; // 10 --> 1
+        second.next = third; // 10 --> 1 --> 8
+        third.next = fourth; // 10 --> 1 --> 8 --> 11
+
+    }
+
+}
