efee2ee4f02c60a76b5ac70596f03e1486478011
dainichi79 <96421043+dainichi79@users.noreply.github.com>
2024-05-30 12:31:28 +0200

    Aggiunto file Read Me

--- src/Singly_Linked_List/Capitolo_012_How_to_find_length_of_a_Singly_Linked_List_Timestamp_04_13/SinglyLinkedList.java
+++ src/Singly_Linked_List/Capitolo_012_How_to_find_length_of_a_Singly_Linked_List_Timestamp_04_13/SinglyLinkedList.java
@@ -1,4 +1,5 @@
-package Singly_Linked_List;
+package Singly_Linked_List.Capitolo_012_How_to_find_length_of_a_Singly_Linked_List_Timestamp_04_13;
+
 public class SinglyLinkedList {
 
     private ListNode head;
@@ -11,6 +12,7 @@ public class SinglyLinkedList {
             this.data = data;
             this.next = null;
         }
+
         @Override
         public String toString() {
             return String.valueOf(data);
@@ -25,20 +27,20 @@ public class SinglyLinkedList {
         }
         System.out.print("null");
     }
-
     public static void main(String[] args) {
-        SinglyLinkedList sll = new SinglyLinkedList();
-        sll.head = new ListNode(10);
+        SinglyLinkedList sl = new SinglyLinkedList();
+        sl.head = new ListNode(10);
         ListNode second = new ListNode(1);
         ListNode third = new ListNode(8);
         ListNode fourth = new ListNode(11);
 
         // Le collego insieme per formare una catena
-        sll.head.next = second; // 10 --> 1
+        sl.head.next = second; // 10 --> 1
         second.next = third; // 10 --> 1 --> 8
-        third.next = fourth; // 10 --> 1 --> 8 --> 11
+        third.next = fourth; // 10 --> 1 --> 8 --> 11 --> null
 
-        sll.display();
+        sl.display();
+
 
     }
 
