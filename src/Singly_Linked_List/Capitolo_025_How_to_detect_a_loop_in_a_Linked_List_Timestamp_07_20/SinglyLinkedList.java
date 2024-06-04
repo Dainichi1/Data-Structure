51dafce660c3693ab609888b07184c26c408059f
dainichi79 <96421043+dainichi79@users.noreply.github.com>
2024-06-04 12:01:57 +0200

    Aggiunto "How to detect a loop in a Linked List"

---
+++ src/Singly_Linked_List/Capitolo_025_How_to_detect_a_loop_in_a_Linked_List_Timestamp_07_20/SinglyLinkedList.java
@@ -0,0 +1,85 @@
+package Singly_Linked_List.Capitolo_025_How_to_detect_a_loop_in_a_Linked_List_Timestamp_07_20;
+
+public class SinglyLinkedList {
+
+    private ListNode head;
+
+    private static class ListNode  {
+        private int data;
+        private ListNode next;
+        public ListNode (int data) {
+            this.data = data;
+            this.next = null;
+        }
+        @Override
+        public String toString() {
+            return String.valueOf(data);
+        }
+    }
+
+    // STAMPO LA LISTA
+    public void display () {
+        ListNode current = head;
+        while (current != null) {
+            System.out.print(current+" ----> ");
+            current = current.next;
+        }
+        System.out.println(""+null);
+    }
+
+    // AGGIUNGO UN ELEMENTO ALL'INIZIO DELLA LISTA
+    public void insertFirst(int value) {
+        ListNode newNode = new ListNode(value);
+        newNode.next = head;
+        head = newNode;
+
+    }
+
+    // CONTROLLO SE C'E' UN LOOP NELLA LISTA
+    public boolean containsLoop () {
+        ListNode fastPointer = head;
+        ListNode slowPointer = head;
+
+        while (fastPointer != null && fastPointer.next != null) {
+            fastPointer = fastPointer.next.next;
+            slowPointer = slowPointer.next;
+
+            if (slowPointer == fastPointer) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // CREO UNA LISTA CON UN LOOP
+    public void createALoopInLinkedList() {
+        ListNode first = new ListNode(1);
+        ListNode second = new ListNode(2);
+        ListNode third = new ListNode(3);
+        ListNode fourth = new ListNode(4);
+        ListNode fifth = new ListNode(5);
+        ListNode sixth = new ListNode(6);
+
+        head = first;
+        first.next = second;
+        second.next = third;
+        third.next = fourth;
+        fourth.next = fifth;
+        fifth.next = sixth;
+        sixth.next = third;
+    }
+
+    public static void main(String[] args) {
+        SinglyLinkedList sll = new SinglyLinkedList();
+
+        sll.createALoopInLinkedList();
+
+        System.out.println(sll.containsLoop());
+
+
+
+
+    }
+
+
+}
