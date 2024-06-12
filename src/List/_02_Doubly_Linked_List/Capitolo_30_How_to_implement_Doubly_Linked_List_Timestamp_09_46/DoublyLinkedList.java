ab64642e37cbbdac4538f0d0f1fe14b04de267ec
dainichi79 <96421043+dainichi79@users.noreply.github.com>
2024-06-12 12:03:30 +0200

    Aggiunto "How to implement Doubly Linked List"

---
+++ src/List/_02_Doubly_Linked_List/Capitolo_30_How_to_implement_Doubly_Linked_List_Timestamp_09_46/DoublyLinkedList.java
@@ -0,0 +1,32 @@
+package List._02_Doubly_Linked_List.Capitolo_30_How_to_implement_Doubly_Linked_List_Timestamp_09_46;
+
+public class DoublyLinkedList {
+
+    private ListNode head;
+    private ListNode tail;
+    private int length;
+
+    private class ListNode {
+        private int data;
+        private ListNode next;
+        private ListNode previous;
+
+        public ListNode(int data) {
+            this.data = data;
+        }
+    }
+
+    public DoublyLinkedList() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    public boolean isEmpty () {
+        return length == 0;
+    }
+
+    public int length() {
+        return length;
+    }
+}
