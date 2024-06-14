8b6375d65fcb665b1b1b469a55d53205ffcae3f8
dainichi79 <96421043+dainichi79@users.noreply.github.com>
2024-06-14 11:50:33 +0200

    Aggiunto "How to implement a Circular Singly Linked List"

---
+++ src/_02_List/_03_Circular_Singly_Linked_List/Capitolo_36_How_to_implement_a_Circular_Singly_Linked_List_Timestamp_11_03/CircularSinglyLinkedList.java
@@ -0,0 +1,49 @@
+package _02_List._03_Circular_Singly_Linked_List.Capitolo_36_How_to_implement_a_Circular_Singly_Linked_List_Timestamp_11_03;
+
+public class CircularSinglyLinkedList {
+
+    private ListNode last;
+    private int length;
+
+    private class ListNode {
+        private ListNode next;
+        private int data;
+
+        public ListNode(int data) {
+            this.data = data;
+        }
+    }
+
+    public CircularSinglyLinkedList() {
+        last = null;
+        length = 0;
+    }
+
+    public int length() {
+        return length;
+    }
+
+    public boolean isEmpty() {
+        return length == 0;
+    }
+
+    public void createCircularLinkedList() {
+        ListNode first = new ListNode(1);
+        ListNode second = new ListNode(5);
+        ListNode third = new ListNode(10);
+        ListNode fourth = new ListNode(15);
+
+        first.next = second;
+        second.next = third;
+        third.next = fourth;
+        fourth.next = first;
+
+        last = fourth;
+    }
+
+    public static void main(String[] args) {
+        CircularSinglyLinkedList csll = new CircularSinglyLinkedList();
+        csll.createCircularLinkedList();
+
+    }
+}
