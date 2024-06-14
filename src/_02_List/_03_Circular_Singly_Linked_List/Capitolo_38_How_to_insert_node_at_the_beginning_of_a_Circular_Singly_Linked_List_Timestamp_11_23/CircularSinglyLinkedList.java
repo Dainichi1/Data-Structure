7830203357e5fe76d808bba96391631e4f383ff1
dainichi79 <96421043+dainichi79@users.noreply.github.com>
2024-06-14 12:23:06 +0200

    Aggiunto "How to insert node at the beginning of a Circular Singly Linked List"

---
+++ src/_02_List/_03_Circular_Singly_Linked_List/Capitolo_38_How_to_insert_node_at_the_beginning_of_a_Circular_Singly_Linked_List_Timestamp_11_23/CircularSinglyLinkedList.java
@@ -0,0 +1,78 @@
+package _02_List._03_Circular_Singly_Linked_List.Capitolo_38_How_to_insert_node_at_the_beginning_of_a_Circular_Singly_Linked_List_Timestamp_11_23;
+
+public class CircularSinglyLinkedList {
+
+    private ListNode last;
+    private int length;
+
+    private class ListNode {
+        private ListNode next;
+        private int data;
+
+        public ListNode(int data) {
+            this.data = data;
+        }
+    }
+
+    public CircularSinglyLinkedList() {
+        last = null;
+        length = 0;
+    }
+
+    public int length() {
+        return length;
+    }
+
+    public boolean isEmpty() {
+        return length == 0;
+    }
+
+    // CREO LA LISTA
+    public void createCircularLinkedList() {
+        ListNode first = new ListNode(1);
+        ListNode second = new ListNode(5);
+        ListNode third = new ListNode(10);
+        ListNode fourth = new ListNode(15);
+
+        first.next = second;
+        second.next = third;
+        third.next = fourth;
+        fourth.next = first;
+
+        last = fourth;
+    }
+
+    // STAMPO LA LISTA
+    public void display() {
+        if (last == null) {
+            return;
+        }
+
+        ListNode first = last.next;
+        while (first != last) {
+            System.out.print(first.data+ " --> ");
+            first = first.next;
+        }
+        System.out.print(first.data);
+    }
+
+    // AGGIUNGO UN NODO ALL'INIZIO DELLA LISTA
+    public void insertFirst (int data) {
+        ListNode temp = new ListNode(data);
+        if (last == null) {
+            last = temp;
+        } else {
+            temp.next = last.next;
+        }
+        last.next = temp;
+        length++;
+    }
+
+    public static void main(String[] args) {
+        CircularSinglyLinkedList csll = new CircularSinglyLinkedList();
+        csll.insertFirst(10);
+        csll.insertFirst(15);
+        csll.insertFirst(25);
+        csll.display();
+    }
+}
