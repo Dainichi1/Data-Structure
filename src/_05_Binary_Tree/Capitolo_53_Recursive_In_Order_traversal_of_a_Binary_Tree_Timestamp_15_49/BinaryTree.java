6c474bc7a4319f75a507d1789d111fb66d8ffd9c
dainichi79 <96421043+dainichi79@users.noreply.github.com>
2024-06-22 12:20:52 +0200

    Aggiunto "Recursive in-ordered traversal of a Binary Tree"

---
+++ src/_05_Binary_Tree/Capitolo_53_Recursive_In_Order_traversal_of_a_Binary_Tree_Timestamp_15_49/BinaryTree.java
@@ -0,0 +1,60 @@
+package _05_Binary_Tree.Capitolo_53_Recursive_In_Order_traversal_of_a_Binary_Tree_Timestamp_15_49;
+
+public class BinaryTree {
+
+    private TreeNode root;
+    private class TreeNode{
+        private TreeNode left;
+        private TreeNode right;
+        private int data;
+
+        public TreeNode(int data) {
+            this.data = data;
+        }
+    }
+
+    // CREO L'ALBERO BINARIO
+    public void createBinaryTree() {
+        TreeNode first = new TreeNode(9);
+        TreeNode second = new TreeNode(2);
+        TreeNode third = new TreeNode(3);
+        TreeNode fourth = new TreeNode(4);
+
+
+        root = first; // root --> first
+        first.left = second;
+        first.right = third; // second <-- first --> third
+
+        second.left = fourth;
+
+
+
+    }
+
+    // ATTRAVERSO IN PROFONDITà L'ALBERO E STAMPO I NUMERI DEI NODI
+    public void inOrder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        inOrder(root.left);
+        System.out.print(root.data+ " ");
+        inOrder (root.right);
+    }
+    public static void main(String[] args) {
+        BinaryTree bt = new BinaryTree();
+        bt.createBinaryTree();
+        bt.inOrder(bt.root);
+    }
+}
+
+/*
+             9
+            /  \
+           /    \
+          2      3
+         / \    null null
+        /   \
+       4    null null
+    null null
+ */
