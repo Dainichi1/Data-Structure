20754f0bdab8b18fca57528438e1514a239abc2e
dainichi79 <96421043+dainichi79@users.noreply.github.com>
2024-06-29 13:10:41 +0200

    Aggiunto "How to represent a Binary Search Tree"

---
+++ src/_05_Tree/_02_Binary_Search_Tree/Capitolo_59_How_to_represemt_a_Binary_Search_Tree_Timestamp_18_48/BinarySearchTree.java
@@ -0,0 +1,21 @@
+package _05_Tree._02_Binary_Search_Tree.Capitolo_59_How_to_represemt_a_Binary_Search_Tree_Timestamp_18_48;;
+
+public class BinarySearchTree {
+
+    private TreeNode root;
+
+    private class TreeNode {
+        private int data;
+        private TreeNode left;
+        private TreeNode right;
+
+        public TreeNode(int data) {
+            this.data = data;
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
