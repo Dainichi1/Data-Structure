5c437ad35b6c13af7aad620c88f2b13ac15e5f0c
dainichi79 <96421043+dainichi79@users.noreply.github.com>
2024-06-29 12:49:28 +0200

    Aggiunto "Recursive find maximum value in a binary tree"

---
+++ src/_05_Binary_Tree/Capitolo_58_Recursive_Find_maximum_value_in_a_Binary_Tree_Timestamp_18_13/BinaryTree.java
@@ -0,0 +1,62 @@
+package _05_Binary_Tree.Capitolo_58_Recursive_Find_maximum_value_in_a_Binary_Tree_Timestamp_18_13;
+
+import java.util.Stack;
+
+public class BinaryTree {
+
+    private TreeNode root;
+    private class TreeNode{
+        private TreeNode left;
+        private TreeNode right;
+        private int data;
+
+        public TreeNode(int data) {
+            this.data = data;
+        }
+    }
+
+    // CREO L'ALBERO BINARIO
+    public void createBinaryTree() {
+        TreeNode first = new TreeNode(1);
+        TreeNode second = new TreeNode(2);
+        TreeNode third = new TreeNode(8);
+        TreeNode fourth = new TreeNode(4);
+        TreeNode fifth = new TreeNode(5);
+        TreeNode sixth = new TreeNode(6);
+        TreeNode seventh = new TreeNode(7);
+
+
+        root = first; // root --> first
+        first.left = second;
+        first.right = third; // second <-- first --> third
+        second.left = fourth;
+        second.right = fifth;
+        third.right = sixth;
+        third.left = seventh;
+    }
+
+    // ATTRAVERSO IN PROFONDITà L'ALBERO E STAMPO I NUMERI DEI NODI
+    public int findMax(TreeNode root) {
+        if (root == null) {
+            return Integer.MIN_VALUE;
+        }
+
+        int result = root.data;;
+        int left = findMax(root.left);
+        int right = findMax(root.right);
+
+        if (left > result) {
+            result = left;
+        }
+        if (right > result) {
+            result = right;
+        }
+        return result;
+        }
+
+    public static void main(String[] args) {
+        BinaryTree bt = new BinaryTree();
+        bt.createBinaryTree();
+        System.out.println(bt.findMax(bt.root));
+    }
+}
